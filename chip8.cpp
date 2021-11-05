d8676b14b3c6717e192ceab51160ae76de1d6647
liam <liam_devlin@live.co.uk>
2021-11-05 15:02:24 +0000

    adding stubs for chip8 interpreter

---
+++ chip8.cpp
@@ -0,0 +1,5 @@
+#include "chip8.h"
+
+chip8::chip8()
+{
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <SDL.h>
+#undef main
+uint32_t SDL_FLAGS;
+SDL_Window* m_Window;
+bool m_ShouldRun = true;
+
+void HandleSDLEvent(SDL_Event& event)
+{
+    switch (event.type)
+    {
+    case SDL_QUIT:
+        m_ShouldRun = false;
+        break;
+    }
+}
 
 int main()
 {
     std::cout << "Hello Chip8" << std::endl;
+    SDL_FLAGS |= SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI;
+    m_Window = SDL_CreateWindow("Chip8 Emu", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 600, SDL_FLAGS);
+
+    while (m_ShouldRun)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            HandleSDLEvent(event);
+        }
+    }
+    return 1;
 }
